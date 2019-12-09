02aba1a3cab70c7d09bb8943b2e29b24889ec278
Ramadhan666 <54971818+Ramadhan666@users.noreply.github.com>
2019-12-09 14:20:19 +0700

    Update anggota.java

--- Perpustakaan/Perpustakaan/anggota.java
+++ Perpustakaan/Perpustakaan/anggota.java
@@ -60,8 +60,8 @@ private void loaddata(){
     }
     private void koneksi(){
         try{
-            Class.forName("com.mysql.jdbo.Driver");
-            Connection con=DriverManager.getConnection("jdbcmysql://127.0.0.1/database_perpustakaan","root", "");
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/database_perpustakaan","root", "");
             stat= con.createStatement();
         }catch(Exception e){
             JOptionPane.showMessageDialog(null, e);
