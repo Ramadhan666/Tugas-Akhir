6a69716e4b7a5cab7fda8c0eab69695e5b45e76e
Ramadhan666 <54971818+Ramadhan666@users.noreply.github.com>
2019-12-09 13:16:59 +0700

    Perpustakaan

---
+++ Perpustakaan/Perpustakaan/SelamatDatang.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Perpustakaan;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author ROG
+ */
+public class SelamatDatang {
+    
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"Selamat Datang di perpustakaan kami");
+        new Login ().setVisible(true);
+    }
+}
